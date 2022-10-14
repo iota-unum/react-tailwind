b6ee2aa0800c0c28821268ebaaf23727e4296712
luigi <luigilamastra@gmail.com>
2022-10-14 06:19:54 +0200

    react with tailwind

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-emerald-500 h-screen w-full text-white flex justify-center items-center 
+    text-5xl">
+      tailwind with create-react-app
     </div>
   );
 }
